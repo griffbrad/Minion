1de9268eb2b1a5270f7d80f46f9cf5b8625448c2
Brad Griffith <griffbrad@gmail.com>
2011-04-17 17:36:49 -0500

    Placeholders for remaining settings sections.
    View helper for settings navigation.

---
+++ library/Minion/View/Helper/SettingsNav.php
@@ -0,0 +1,45 @@
+<?php
+
+class Minion_View_Helper_SettingsNav extends Zend_View_Helper_Abstract
+{
+    private $_active;
+
+    public function settingsNav($active)
+    {
+        $this->_active = $active;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        $pages = array(
+            'settings'               => 'General',
+            'settings-users'         => 'Users',
+            'settings-notifications' => 'Notifications',
+            'settings-tasks'         => 'Tasks',
+            'settings-servers'       => 'Servers'
+        );
+
+        $out = '<ul id="minion-settings-navigation">';
+
+        foreach ($pages as $page => $title) {
+            $class = '';
+
+            if ($this->_active === $page) {
+                $class = 'selected'; 
+            } 
+            
+            $out .= sprintf(
+                '<li class="%s"><a href="%s.php">%s</a></li>',
+                $class,
+                $page,
+                $title
+            );
+        }
+
+        $out .= '</ul>';
+
+        return $out;
+    } 
+}
