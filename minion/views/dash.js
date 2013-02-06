ee834fa74a81f7eaec1e73fa30698e8fe9a17947
Brad Griffith <griffbrad@gmail.com>
2013-02-06 11:10:37 -0600

    Some basic notification stats for dash.

--- minion/views/dash.js
+++ minion/views/dash.js
@@ -11,9 +11,14 @@ util.inherits(Dash, View);
 module.exports = Dash;
 
 Dash.prototype.init = function () {
-    var date = this.getQuery('date'),
-        self = this;
+    var date = new Date(Date.parse(this.getQuery('date')));
 
+    this.findCheckStats(date);
+};
+
+Dash.prototype.findCheckStats = function (date) {
+    var self = this;
+    
     this._minion.getDb().collection('sites', function (err, collection) {
         collection
             .find()
@@ -28,6 +33,55 @@ Dash.prototype.init = function () {
                         self._errors.push(item.url + ': ' + item.reason);
                     }
                 }
+
+                self.findDailyNotificationStats(date);
+            });
+    });
+};
+
+Dash.prototype.findDailyNotificationStats = function (date) {
+    var self = this,
+        daily = new Date(date.getTime()),
+        next  = new Date(daily.getTime());
+
+    next.setDate(daily.getDate() + 1);
+
+    this._minion.getDb().collection('notifications', function (err, collection) {
+        collection
+            .find({
+                dateSent: {
+                    '$gte': daily,
+                    '$lt':  next
+                }
+            })
+            .toArray(function (err, items) {
+                self._dailyNotificationCount = items.length;
+                self.findMonthlyNotificationStats(date);
+            });
+    });
+};
+
+Dash.prototype.findMonthlyNotificationStats = function (date) {
+    var self  = this,
+        month = new Date(date.getTime()),
+        next;
+   
+    month.setDate(0);
+    
+    next = new Date(month.getTime());
+    next.setMonth(month.getMonth() + 1);
+    next.setDate(0);
+
+    this._minion.getDb().collection('notifications', function (err, collection) {
+        collection
+            .find({
+                dateSent: {
+                    '$gte': month,
+                    '$lt':  next
+                }
+            })
+            .toArray(function (err, items) {
+                self._monthlyNotificationCount = items.length;
 
                 self.initComplete();
             });
@@ -49,6 +103,24 @@ Dash.prototype.get = function () {
                 type:   'daily',
                 title:  'Checks',
                 value:  this._checkCount,
+                format: 'integer'
+            },
+            {
+                type:   'daily',
+                title:  'Notifications',
+                value:  this._dailyNotificationCount,
+                format: 'integer'
+            },
+            {
+                type:   'monthly',
+                title:  'Checks',
+                value:  this._checkCount,
+                format: 'integer'
+            },
+            {
+                type:   'monthly',
+                title:  'Notifications',
+                value:  this._monthlyNotificationCount,
                 format: 'integer'
             }
         ]
